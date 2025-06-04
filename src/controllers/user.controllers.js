b11110e6057235dd6e5a25c801757f65b05ffb37
priya kuamri <webygirlz@gmial.com>
2025-06-04 12:57:01 +0530

    add img controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -180,7 +180,7 @@ const refreshAccessToken = asyncHandler(async (req,res)=>{
         throw new ApiError(401,"invalid refresh token")
     }
 
-    if(incomingRefreshToken !== user.refreshToken){
+    if(!incomingRefreshToken !== user.refreshToken){
         throw new ApiError(401,"Refresh token is expired or used")
     }
     const options ={
@@ -201,10 +201,90 @@ const refreshAccessToken = asyncHandler(async (req,res)=>{
    }
 })
 
+// change password logic
+
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"old password is not correct")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200,{},"password change successfully"))
+})
+
+
+// get user details
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+   return res.status(200).json(200,req.user,"current user fetch successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName,email} = req.body
+
+    if(!(fullName || email)){
+        throw new ApiError(400,"All fields are required")
+    };
+
+    const user = await User.findByIdAndUpdate(req.user?._id,{$set:{fullName,email}},{new:true}).select(".password")
+    return res.status(200).json(new ApiError(200,user,"Account details updated successfully"))
+})
+
+// update user aavatar
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+    const avatarLocalPath = req.file?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Please upload a file Avatar file os missing")
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading on avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,{$set:{avatar:avatar.url}},{new:true}).select("-password")
+
+    return res.status(200).json(new ApiResponse(200,user,"Avatar image updated successfully"))
+})
+
+
+
+
+// update cover image
+const updateUserCoverImgae = asyncHandler(async(req,res)=>{
+    const converImageLocalPath = req.file?.path
+
+    if(!converImageLocalPath){
+        throw new ApiError(400,"Please upload a file converImage....file is missing")
+    }
+    const converImage = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!converImage.url){
+        throw new ApiError(400,"Error while uploading on avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,{$set:{avatar:avatar.url}},{new:true}).select("-password")
+
+     return res.status(200).json(new ApiResponse(200,user,"cover image updated successfully"))
+})
+
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImgae
 }
---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+
+    subscriber:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    channel:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+    },{timestamps:true})
+
+export const subscription = mongoose.model("subscription",subscriptionSchema);
